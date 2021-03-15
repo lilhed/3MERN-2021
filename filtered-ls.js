f6084e350ae6a606516e1cbc2a9f350079038a38
hwitosz <hugo.witosz@webdealauto.com>
2021-03-15 13:25:15 +0100

    test

--- filtered-ls.js
+++
@@ -1,8 +0,0 @@
-require('fs').readdir(
-    process.argv[2], (err, list) => {
-      if (err) return console.err(err);
-      list
-        .filter(file => require('path').extname(file) === '.' + process.argv[3])
-        .forEach(filterdFile => console.log(filterdFile))
-    }
-  )
--- my-first-async-io.js
+++
@@ -1,4 +0,0 @@
-require('fs').readFile(process.argv[2], (err, data) => {
-    if (err) return console.err(err);
-    console.log(data.toString().split('\n').length - 1);
-  })
