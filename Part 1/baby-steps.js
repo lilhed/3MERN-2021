9a9c3bba4961428c7b23db6903e9eb02ed215b1b
Pypajiel <74911363+Pypajiel@users.noreply.github.com>
2021-03-15 13:48:00 +0100

    Delete baby-steps.js

--- Part 1/baby-steps.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const numbers = process.argv.splice(2);
-
-const sum = numbers.reduce((previousValue, currentValue) => {
-    return previousValue + parseInt(currentValue);
-}, 0);
-
-console.log(sum);
