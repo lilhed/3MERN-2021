8cf55d4b9bde0df67b38eb25e292737412d436ad
Ghullx <291493@supinfo.com>
2021-03-15 13:46:48 +0100

    Exercices 1-2-3-4-5-6-7-8

---
+++ Part 1/LearnYouNode/exercices/07_http_client.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const url = process.argv[2];
+
+http.get(url, function (response){
+    response.setEncoding('utf-8')
+
+    response.on('error', function (err){
+        console.log(err);
+    });
+
+    response.on('data', function (data){
+        console.log(data)
+    })
+})
