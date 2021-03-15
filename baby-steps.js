42c74b2c8c8418da2aaf8b742944c868c44e7cd9
Sad0017 <theo.bezin@supinfo.com>
2021-03-15 12:03:05 +0100

    exo1,2,3,4,5

---
+++ baby-steps.js
@@ -0,0 +1,10 @@
+
+var somme = 0;
+
+for (var i = 2; i < process.argv.length; i++) {
+    
+    somme += Number(process.argv[i]);
+}
+
+console.log(somme);
+
---
+++ my-first-async-io.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+fs.readFile(process.argv[2], function (err,data){
+    if(err){
+        
+    }else{
+        var str = data.toString();
+        var strArray = str.split('\n');
+        console.log(strArray.length - 1);
+    };
+});
+
