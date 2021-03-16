598e138ef5c4bc76c412012883c431a7f4c13be1
Zaiden13 <louan.leplae@gmail.com>
2021-03-16 15:51:57 +0100

    mogon ex 4

---
+++ Part 2/LearnYouMongo/exercices/02_ex4.js
@@ -0,0 +1,14 @@
+const mongo = require('mongodb').MongoClient;
+const dbUrl = 'mongodb://localhost:27017/';
+const dbName = 'learnyoumongo';
+const [ ,, age ] = process.argv;
+
+mongo.connect(dbUrl, { useUnifiedTopology: true }).then((client) => {
+    const db = client.db(dbName);
+    db.collection('parrots')
+    .find({ age: { $gt: Number(age) }}, { projection:{ _id: 0, name: 1, age: 1 }})
+    .toArray().then(docs => {
+        console.log(docs);
+        client.close().then();
+    }).catch(console.error);
+}).catch(err => { throw err});
