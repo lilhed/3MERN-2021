e5733cc4f4a7a28d9da1f652d055465dd68cbddd
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2021-03-16 16:11:14 +0100

    Ex 05

---
+++ Part 2/LearnYouMongo/05_Insert_into.js
@@ -0,0 +1,18 @@
+const mongo = require('mongodb').MongoClient;
+const dbUrl = 'mongodb://localhost:27017/';
+const dbName = 'learnyoumongo';
+const [ ,, firstName, lastName ] = process.argv;
+
+mongo.connect(dbUrl, { useUnifiedTopology: true }).then((client) => {
+    const db = client.db(dbName);
+    const users = db.collection('docs');
+    const newUser = { 'firstName': firstName, 'lastName': lastName };
+
+    users.insertOne(newUser).then(result => {
+        console.log(JSON.stringify(newUser));
+        client.close().then();
+    }).catch(err => {
+        console.error(err);
+        client.close().then();
+    });
+}).catch(console.error);
