2f814fbc28b82391b2d94d1dd73c3c5826a117f5
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2021-03-16 15:49:24 +0100

    Ex 04

---
+++ Part 2/LearnYouMongo/04_Select_which_fields_to_get.js
@@ -0,0 +1,14 @@
+const mongo = require('mongodb').MongoClient;
+const dbUrl = 'mongodb://localhost:27017/';
+const dbName = 'learnyoumongo';
+const [ ,, age ] = process.argv;
+
+mongo.connect(dbUrl, { useUnifiedTopology: true }).then((client) => {
+    const db = client.db(dbName);
+    const parrots = db.collection('parrots');
+
+    parrots.find({ age: { $gt: Number(age) }}, { projection: { _id: 0, name: 1, age: 1 }}).toArray().then(docs => {
+        console.log(docs);
+        client.close().then();
+    }).catch(console.error);
+});
