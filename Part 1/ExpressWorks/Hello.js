5a8c5bbb91535e9b637c5d1f6e2580de4bc1f8bb
Pypajiel <74911363+Pypajiel@users.noreply.github.com>
2021-03-16 23:11:30 +0100

    Add files via upload

---
+++ Part 1/ExpressWorks/Hello.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+var portNumber = process.argv[2];
+
+app.get('/home', function(req, res) {
+    res.end('Hello World!')
+});
+
+app.listen(portNumber);
