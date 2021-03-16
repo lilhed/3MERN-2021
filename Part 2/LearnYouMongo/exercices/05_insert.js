65706e16f41184e3e5c49bd6dadc7066da231a57
Ghullx <291493@supinfo.com>
2021-03-16 16:12:55 +0100

    Exercice 1-2-3-4-5

--- Part 2/LearnYouMongo/exercices/05_insert.js
+++ Part 2/LearnYouMongo/exercices/05_insert.js
@@ -4,13 +4,16 @@ const dbName = 'learnyoumongo';
 
 const firstName = process.argv[2];
 const lastName = process.argv[3];
+const doc = { firstName: firstName, lastName: lastName };
 
 mongo.connect(url, { useUnifiedTopology: true }).then((client) => {
     const db = client.db(dbName);
-    db.collection('docs')
-        .insertOne({firstName, lastName}, function (err, documents) {
-            if (err) return console.log(err);
-            console.log(JSON.stringify({firstName, lastName}));
-            client.close().then();
-        }).catch(err => { throw err});
+    const docs = db.collection('docs');
+    docs.insertOne(doc).then(result => {
+        console.log(JSON.stringify(doc));
+        client.close().then();
+    }).catch(err => {
+        console.error(err);
+        client.close().then();
+    });
 }).catch(err => { throw err});
