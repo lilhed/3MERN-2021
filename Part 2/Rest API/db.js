59c28225c47eca62432f8d5788ac639766a0ecc4
Francois Leclercq <francois.leclercq@supinfo.com>
2021-03-17 11:44:48 +0100

    Exercices Day3

---
+++ Part 2/Rest API/db.js
@@ -0,0 +1,4 @@
+var mongoose = require('mongoose');
+//const { MongoClient } = require('mongodb');
+mongoose.connect('mongodb+srv://3merncluster.h4gai.mongodb.net/myFirstDatabase');
+//MongoClient.connect('mongodb+srv://3merncluster.h4gai.mongodb.net/myFirstDatabase', {useUnifiedTopology: true } );
