b3e539b56ac6bc70490e7252ec5c1f840156f3f2
Timothée Lefebvre <292261@supinfo.com>
2021-03-17 11:47:47 +0100

    Exercices rest api

---
+++ Part 2/Rest API/exercice/routes/posts.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/Post");
+
+router.get('/posts', (req,res) => {
+    res.send('Voici les posts !');
+})
+
+
+
+router.post('/', (req,res) => {
+    console.log(req.body)
+})
+
+
+module.exports = router;
