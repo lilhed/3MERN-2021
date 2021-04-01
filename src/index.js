10ac7957915fa11541af4f1102ad5285ca9a81d9
Michel Obame <Michel241>
2021-04-01 15:19:51 +0200

    site commit

--- src/index.js
+++ src/index.js
@@ -4,14 +4,47 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, Move, MoveIn, MoveOut, Sticky, StickyIn, ZoomIn } from "react-scroll-motion";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Presenter = () => {
+  const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
+  const FadeUp = batch(Fade(), Move(), Sticky());
+
+  return (
+    <ScrollContainer>
+      <ScrollPage page={0}>
+        <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -200))}>
+          <span style={{ fontSize: "3em" }}>Let't me show you scroll animation 😀</span>
+        </Animator>
+      </ScrollPage>
+      <ScrollPage page={1}>
+        <Animator animation={ZoomInScrollOut}>
+          <span style={{ fontSize: "3em" }}>I'm FadeUpScrollOut ✨</span>
+        </Animator>
+      </ScrollPage>
+      <ScrollPage page={2}>
+        <Animator animation={FadeUp}>
+          <span style={{ fontSize: "3em" }}>I'm FadeUp ⛅️</span>
+        </Animator>
+      </ScrollPage>
+      <ScrollPage page={3}>
+        <div style={FlexCenterStyle}>
+          <span style={{ fontSize: "3em" }}>
+            <Animator animation={MoveIn(-1000, 0)}>Hello Guys 👋🏻</Animator>
+            <Animator animation={MoveIn(1000, 0)}>Nice to meet you 🙋🏻‍♀️</Animator>- I'm Seonghyeok -
+            <Animator animation={MoveOut(1000, 0)}>Good bye ✋🏻</Animator>
+            <Animator animation={MoveOut(-1000, 0)}>See you 💛</Animator>
+          </span>
+        </div>
+      </ScrollPage>
+      <ScrollPage page={4}>
+        <Animator animation={batch(Fade(), Sticky())}>
+          <span style={{ fontSize: "3em" }}>Done</span>
+          <span style={{ fontSize: "3em" }}>
+            There's FadeAnimation, MoveAnimation, StickyAnimation, ZoomAnimation
+          </span>
+        </Animator>
+      </ScrollPage>
+    </ScrollContainer>
+  );
+};
